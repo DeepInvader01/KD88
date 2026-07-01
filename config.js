fd0c4c7c5224a28786ee7ad9969451d27812d71b
DeepInvader01 <152061900+DeepInvader01@users.noreply.github.com>
2026-07-01 12:49:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ const CONFIG = {
   // Dashboard cards "Total Players over 25M" and "Below 70% Total Goal over 25M"
   // ignore players below this Power value.
   activePlayerThreshold: 25000000,
+  
+  defaultDataset: "KvK7_Start",
 
   datasets: [
     { label: "KvK6 • Total", sheetName: "KvK6_Total" },
